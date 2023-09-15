76eb99d190815052c851fdcd63f5b956504c426a
Junaid Ahmed <junaidahmed@teamitg.com>
2023-09-15 15:05:42 +0100

    feat(infrastructure): Network Load Balancer

---
+++ terraform/02-network-loadbalancer/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  type        = string
+  description = "Project name"
+}
+
+variable "compartment_id" {
+  type        = string
+  description = "The compartment to create the resources in"
+}
+
+variable "region" {
+  type        = string
+  description = "The region to provision the resources in"
+}
+
+variable "main_instance_ocid" {
+  type        = string
+  description = "Instance OCID in node pool where traefik is deployed"
+}
